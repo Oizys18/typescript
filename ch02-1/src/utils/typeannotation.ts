75aca9c8c8f689368df9c57912cef576ebd53f6b
Oizys18 <oizys18@gmail.com>
2020-10-13 23:45:12 +0900

    Mod type assertion

--- ch02-1/src/utils/typeannotation.ts
+++
@@ -1,6 +0,0 @@
-// let 변수이름: 타입   (초깃값 안줘도 됨 )
-// let 변수이름: 타입 = 초깃값  
-let variable1: string 
-
-// const 변수이름: 타입 = 초깃값 
-const var2: number = 1
--- ch03/03-5_Object_type_conversion.ts
+++ ch03/03-5_Object_type_conversion.ts
@@ -1,5 +1,5 @@
 // 객체타입변환 (type assertion)
-
+// 바로 접근해서 타입 변환하는 방법! 
 export default interface INameable {
   name: string;
 }
@@ -8,6 +8,7 @@ export default interface INameable {
 let obj:object = {name:'Jack'}
 let name1 = (<INameable>obj).name
 let name2 = (obj as INameable).name
-// console.log(obj.name)은 에러가 난다! 왜? obj는 name 속성을 가지고 있지 않기 때문! 
+// console.log(obj.name)은 에러가 난다! 
+// 왜? obj는 name 속성을 가지고 있지 않기 때문! 
 console.log(name1,name2) 
 
